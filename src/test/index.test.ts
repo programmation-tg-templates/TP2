35c044f554f5569ef40a2ca5df7bbe1e9b4a2c7b
François ROLAND <francois.roland@heh.be>
2025-09-28 16:44:30 +0200

    Import Gianni's tests.
    Added tests for letter case verification.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,15 @@
-// Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
+import {verifierLettre} from '../index';
 
-import {...} from '../index';
+describe('Vérifier la casse d\'une lette', () => {
+    test('Tester que ce n\'est pas un caractère', () => {
+        expect(verifierLettre('AA')).toEqual('invalide');
+    });
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+    test('Tester une lettre majuscule', () => {
+        expect(verifierLettre('A')).toEqual('majuscule');
+    });
+
+    test('Tester une lettre minuscule', () => {
+        expect(verifierLettre('a')).toEqual('minuscule');
     });
 });
